767a4ce1167ec5d8a33b5d13feb93b00e4bee267
Sergey Sviridov <ssviridov@techmategroup.com>
2022-08-07 23:07:07 +0400

    minor fixes

--- src/test/java/com/demoqa/StudentRegistrationFormTest.java
+++ src/test/java/com/demoqa/StudentRegistrationFormTest.java
@@ -4,8 +4,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -26,7 +24,7 @@ public class StudentRegistrationFormTest {
         $("#firstName").setValue("Sergey");
         $("#lastName").setValue("Sviridov");
         $("#userEmail").setValue("super@test.com");
-        $("[for = gender-radio-1]").click();
+        $(byText("Male")).click();
         $("#userNumber").setValue("9171234567");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
@@ -35,8 +33,8 @@ public class StudentRegistrationFormTest {
         $(".react-datepicker__year-select").selectOptionByValue("1993");
         $(".react-datepicker__day--008").click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
-        $("[for = hobbies-checkbox-1]").click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/face.png"));
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("face.png");
         $("#currentAddress").setValue("960 AVENUE OF THE AMERICAS NEW YORK");
         $("#state").click();
         $(byText("Haryana")).click();
@@ -45,15 +43,15 @@ public class StudentRegistrationFormTest {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Sergey Sviridov"));
-        $(".table-responsive").shouldHave(text("super@test.com"));
-        $(".table-responsive").shouldHave(text("Male"));
-        $(".table-responsive").shouldHave(text("9171234567"));
-        $(".table-responsive").shouldHave(text("08 October,1993"));
-        $(".table-responsive").shouldHave(text("Computer Science"));
-        $(".table-responsive").shouldHave(text("Sports"));
-        $(".table-responsive").shouldHave(text("face.png"));
-        $(".table-responsive").shouldHave(text("960 AVENUE OF THE AMERICAS NEW YORK"));
-        $(".table-responsive").shouldHave(text("Haryana Panipat"));
+        $(".table-responsive").find(byText("Student Name")).closest("tr").shouldHave(text("Sergey Sviridov"));
+        $(".table-responsive").find(byText("Student Email")).closest("tr").shouldHave(text("super@test.com"));
+        $(".table-responsive").find(byText("Gender")).closest("tr").shouldHave(text("Male"));
+        $(".table-responsive").find(byText("Mobile")).closest("tr").shouldHave(text("9171234567"));
+        $(".table-responsive").find(byText("Date of Birth")).closest("tr").shouldHave(text("08 October,1993"));
+        $(".table-responsive").find(byText("Subjects")).closest("tr").shouldHave(text("Computer Science"));
+        $(".table-responsive").find(byText("Hobbies")).closest("tr").shouldHave(text("Sports"));
+        $(".table-responsive").find(byText("Picture")).closest("tr").shouldHave(text("face.png"));
+        $(".table-responsive").find(byText("Address")).closest("tr").shouldHave(text("960 AVENUE OF THE AMERICAS NEW YORK"));
+        $(".table-responsive").find(byText("State and City")).closest("tr").shouldHave(text("Haryana Panipat"));
     }
 }
